7702251fa1883d119d9c229200ddcc1c69529ef7
Daniel Gallichan <dgallichan@yahoo.com>
2023-07-31 15:46:30 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,35 @@ input.onButtonPressed(Button.A, function () {
         . # . # .
         `)
 })
+function fixString (numToPad: number) {
+    numDigits = 5
+    thisText = convertToText(numToPad)
+    thisLength = thisText.length
+    newText = ""
+    if (thisLength < numDigits) {
+        if (thisText.substr(0, 1).compare("-") == 0) {
+            newText = "-"
+            for (let index = 0; index < numDigits - thisLength; index++) {
+                newText = "" + newText + "0"
+            }
+            newText = "" + newText + thisText.substr(1, thisLength - 1)
+        } else {
+            for (let index = 0; index < numDigits - thisLength; index++) {
+                newText = "" + newText + "0"
+            }
+            newText = "" + newText + thisText
+        }
+    } else if (thisLength > numDigits) {
+        newText = thisText.substr(0, numDigits)
+    } else {
+        newText = thisText
+    }
+    return newText
+}
+let newText = ""
+let thisLength = 0
+let thisText = ""
+let numDigits = 0
 radio.setGroup(99)
 basic.showLeds(`
     . . . . .
@@ -17,8 +46,7 @@ basic.showLeds(`
     . # . # .
     `)
 basic.forever(function () {
-    radio.sendString("1" + "/" + input.acceleration(Dimension.X) + "/" + input.acceleration(Dimension.Y) + "/" + input.acceleration(Dimension.Z) + ";")
-    control.waitMicros(20)
-    radio.sendString("2" + "/" + Math.round(input.magneticForce(Dimension.X) * 10) / 10 + "/" + Math.round(input.magneticForce(Dimension.Y) * 10) / 10 + "/" + Math.round(input.magneticForce(Dimension.Z) * 10) / 10 + ";")
-    control.waitMicros(20)
+    radio.sendString("1" + "/" + fixString(input.acceleration(Dimension.X)) + "/" + fixString(input.acceleration(Dimension.Y)) + "/" + fixString(input.acceleration(Dimension.Z)) + ";")
+    basic.pause(5)
+    radio.sendString("2" + "/" + fixString(input.magneticForce(Dimension.X)) + "/" + fixString(input.magneticForce(Dimension.Y)) + "/" + fixString(input.magneticForce(Dimension.Z)) + ";")
 })
