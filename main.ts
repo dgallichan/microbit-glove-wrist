892e65120538899b6420852b7e0e446b9b313af1
Daniel Gallichan <dgallichan@yahoo.com>
2023-07-31 14:45:38 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -19,6 +19,6 @@ basic.showLeds(`
 basic.forever(function () {
     radio.sendString("1" + "/" + input.acceleration(Dimension.X) + "/" + input.acceleration(Dimension.Y) + "/" + input.acceleration(Dimension.Z) + ";")
     control.waitMicros(20)
-    radio.sendString("2" + "/" + Math.round(input.magneticForce(Dimension.X) * 10 / 10) + "/" + Math.round(input.magneticForce(Dimension.Y) * 10 / 10) + "/" + Math.round(input.magneticForce(Dimension.Z) * 10 / 10) + ";")
+    radio.sendString("2" + "/" + Math.round(input.magneticForce(Dimension.X) * 10) / 10 + "/" + Math.round(input.magneticForce(Dimension.Y) * 10) / 10 + "/" + Math.round(input.magneticForce(Dimension.Z) * 10) / 10 + ";")
     control.waitMicros(20)
 })
